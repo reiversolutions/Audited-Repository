15722e0a832ed8c6c4bb55512b16209510508fa6
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2015-02-22 16:39:06 +0000

    Concrete classes made so that they can be referenced

---
+++ AuditedRepository/Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using AuditedRepository.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditedRepository.Models
+{
+    /// <summary>
+    /// Entity base model
+    /// </summary>
+    public class Entity : MaintenanceProperties, IEntity
+    {
+        public string Id { get; set; }
+    }
+}
---
+++ AuditedRepository/Models/MaintenanceProperties.cs
@@ -0,0 +1,20 @@
+﻿using AuditedRepository.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditedRepository.Models
+{
+    /// <summary>
+    /// Maintenance properties only
+    /// </summary>
+    public class MaintenanceProperties : IMaintenanceProperties
+    {
+        public bool IsArchived { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
