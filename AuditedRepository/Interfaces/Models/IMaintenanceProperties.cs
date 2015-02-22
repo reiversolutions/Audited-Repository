ed52b2ca21160251e366cc89857f62440591a9d5
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2015-02-22 12:51:41 +0000

    Maintenance properties allow soft deletes and simple file auditing

---
+++ AuditedRepository/Interfaces/Models/IMaintenanceProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditedRepository.Interfaces.Models
+{
+    public interface IMaintenanceProperties
+    {
+        bool IsArchived { get; set; }
+        DateTime ModifiedDate { get; set; }
+        DateTime CreatedDate { get; set; }
+    }
+}
---
+++ AuditedRepository/Models/MaintenanceProperties.cs
@@ -0,0 +1,16 @@
+﻿using AuditedRepository.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditedRepository.Models
+{
+    public class MaintenanceProperties : IMaintenanceProperties
+    {
+        public bool IsArchived { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
