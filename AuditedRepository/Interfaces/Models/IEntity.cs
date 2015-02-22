60c06c53e989deb4a9a418b3e71956dfdd0a8265
Daniel Charlton <charlton.dan@reiversolutions.co.uk>
2015-02-22 14:14:08 +0000

    Entity interface

---
+++ AuditedRepository/Interfaces/Models/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditedRepository.Interfaces.Models
+{
+    public interface IEntity : IMaintenanceProperties
+    {
+        /// <summary>
+        /// Primary key
+        /// </summary>
+        string Id { get; set; }
+    }
+}
--- AuditedRepository/Interfaces/Models/IMaintenanceProperties.cs
+++ AuditedRepository/Interfaces/Models/IMaintenanceProperties.cs
@@ -8,8 +8,17 @@ namespace AuditedRepository.Interfaces.Models
 {
     public interface IMaintenanceProperties
     {
+        /// <summary>
+        /// Has been archived
+        /// </summary>
         bool IsArchived { get; set; }
+        /// <summary>
+        /// Last modified date
+        /// </summary>
         DateTime ModifiedDate { get; set; }
+        /// <summary>
+        /// Date entity was created
+        /// </summary>
         DateTime CreatedDate { get; set; }
     }
 }
